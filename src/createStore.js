b61ff2efcb036bce8d26f8323762ac793b5bb054
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 07:04:19 -0500

    imports { render } instead of { renderer }

--- src/createStore.js
+++ src/createStore.js
@@ -1,20 +1,23 @@
-import { renderer }  from './index.js'
+import { render }  from './index.js';
 
 export default function createStore(reducer){
-    let state;
+  let state;
 
-    function dispatch(action){
-      state = reducer(state, action)
-      console.log(`the action is`)
-      console.log(action)
-      console.log(`the state is`)
-      console.log(state)
-      renderer.render()
-    }
+  function dispatch(action) {
+    state = reducer(state, action);
+    console.log(`the action is`);
+    console.log(action);
+    console.log(`the state is`);
+    console.log(state);
+    render();
+  };
 
-    function getState(){
-      return state;
-    }
+  function getState(){
+    return state;
+  };
 
-    return {dispatch: dispatch, getState: getState}
-  }
+  return {
+    dispatch, 
+    getState
+  };
+};
