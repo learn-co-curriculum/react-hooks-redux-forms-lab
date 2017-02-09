ea9832b1fb6eecd5f340ab5f95e58b6debb39341
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 07:57:25 -0500

    ensure console is not run during test of createStore.js

--- src/createStore.js
+++ src/createStore.js
@@ -1,15 +1,17 @@
-import { render }  from './index.js';
+import { renderer } from './index.js';
 
 export default function createStore(reducer){
   let state;
 
   function dispatch(action) {
     state = reducer(state, action);
-    console.log(`the action is`);
-    console.log(action);
-    console.log(`the state is`);
-    console.log(state);
-    render();
+    if (process.env.NODE_ENV !== 'test') {
+      console.log(`the action is`);
+      console.log(action);
+      console.log(`the state is`);
+      console.log(state);
+    }
+    renderer.render()
   };
 
   function getState(){
