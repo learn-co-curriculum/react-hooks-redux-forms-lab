7d7e0cd9cd3c0f1886cb61f7e0fec3182857a0b6
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 07:53:24 -0500

    import App Component directly in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import App from './App';
 import createStore from './createStore'
 import manageBand from './reducers/manageBand'
 
